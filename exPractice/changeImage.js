4a021ccc37f89a865575f62a089e3aa190e7e03f
msj <audtmdwjd@naver.com>
2023-07-25 09:34:57 +0900

    연락처 관리

---
+++ exPractice/changeImage.js
@@ -0,0 +1,27 @@
+const ul = document.querySelector("ul");
+const p = document.querySelector("p img");
+
+
+// console.log(p.src);
+
+// ul.addEventListener("click",(e) => {
+// const show = e.target.closest("a");
+// p.src = a.href
+
+// p.src = e.target.href;
+// e.preventDefault();
+})
+
+
+// thumbs.onclick = function(event) {
+//   let thumbnail = event.target.closest('a');
+
+//   if (!thumbnail) return;
+//   showThumbnail(thumbnail.href, thumbnail.title);
+//   event.preventDefault();
+// }
+
+// function showThumbnail(href, title) {
+//   largeImg.src = href;
+//   largeImg.alt = title;
+// }
---
+++ exPractice/numberManagement.js
@@ -0,0 +1,43 @@
+
+const nameInput = document.querySelector("div input:nth-of-type(1)");
+const phone = document.body.querySelector("div input:nth-of-type(2)");
+const email = document.body.querySelector("div input:nth-of-type(3)");
+const addBtn = document.querySelector("button:nth-of-type(1)");
+const removeBtn = document.querySelector("#remove");
+const removeInput = document.querySelector("#removeContactName");
+
+const arr = [];
+addBtn.addEventListener("click", () => {
+ const li = document.createElement("li");
+//  li.textContent = `${nameInput.value},${phone.value},${email.value}`;
+user = {
+  name: nameInput.value,
+  phone: phone.value,
+  email: email.value
+}
+arr.push(user)
+li.innerHTML = `${user.name} | ${user.phone} | ${user.email}`;
+// li.setAttribute("liUser", JSON.stringify(user));
+ document.querySelector("ul").prepend(li);
+ li.setAttribute("data-name",user.name)
+})
+
+
+
+removeBtn.addEventListener("click",() => {
+const li = document.querySelectorAll("li")
+li.forEach(li => {
+  if(li.getAttribute("data-name") === removeInput.value){
+    li.remove();
+  }
+});
+
+// arr = arr.filter((arr) => arr.name === removeInput.value);
+
+// arr.forEach(arr => {
+//     // const liContactInfo = JSON.parse(li.getAttribute("liUser"));
+//     if(arr.name === removeInput.value){
+//       li.remove();
+//     }
+//   });
+})
